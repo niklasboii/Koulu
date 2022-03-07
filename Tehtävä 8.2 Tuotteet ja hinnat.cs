91f87cb04cce2de65c9fec1dfc00822a765445b7
niklasboii <98732864+niklasboii@users.noreply.github.com>
2022-03-07 11:16:18 +0000

    Add files via upload

---
+++ Tehtävä 8.2 Tuotteet ja hinnat.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.Write("Tehtävä 8.2");
+Console.WriteLine();
+
+Console.Write("");
+Console.WriteLine();
+
+Console.Write("----------------");
+Console.WriteLine();
+
+Console.Write("");
+Console.WriteLine();
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // jotta tunnistii € merkin muuten ilman hettää ? merkin
+
+String[] tuotteet = { "Vasara", "Saha", "Kirves" }; 
+
+int[] hinnat = {5,10,15};
+
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(tuotteet[i] + " " + hinnat[i] + " €"); // tuotteet - väli -tuotteiden hinnat - väli sekä € merkki
+}
