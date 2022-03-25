43ec22bd1abfa08e09f6cfb82d68d97a685bc73d
niklasboii <98732864+niklasboii@users.noreply.github.com>
2022-03-25 11:33:02 +0000

    Tehtävä 14 syötteen tarkistus

---
+++ Tehtävä 14 syötteen tarkistus.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Tehtävä 14 (N.Eloranta)");
+
+Console.WriteLine("------------------------");
+
+Console.WriteLine();
+
+int A = 0;
+bool res = false;
+string vastaus = null;
+
+Console.Write("Anna kokonaisluku: ");
+vastaus = Console.ReadLine();
+
+res = int.TryParse(vastaus, out A);
+
+if (res == true)
+{
+Console.WriteLine("Converted '" + vastaus + "' to " + A + ".    JEE!");
+}
+
+else
+{
+    Console.WriteLine("Attempted conversion of '" + vastaus + "' failed.    SORRY!");
+}
+
+
